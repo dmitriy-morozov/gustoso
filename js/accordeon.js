af2d623a1e3b4e031bb0b33fc7d5e0478ad5b795
Dmitriy Morozov <dmitriy_morozov@mail.ua>
2017-02-08 00:07:32 +0200

    Update accordeon.js

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,4 +1,9 @@
- $('#accordeon-item--first').click(function() {
+$(document).ready(function()  {
+	   $(".accordeon-item--first").css('z-index', '5');
+	   $(".accordeon-item--second").css('z-index', '2');
+       $(".accordeon-item--third").css('z-index', '1');
+	}); 
+$('#accordeon-item--first').click(function() {
             if($("#accordeon-item--first").is(':checked')) { 
                $(".accordeon-item--first").css('z-index', '5');
                $(".accordeon-item--second").css('z-index', '2');
