5b6cbc34bec93140bdd7af8720ec71c3a3d5c811
dmitriy-morozov <dimokin777@mail.ru>
2017-02-07 17:44:25 +0200

    edit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Лэндинг Gustoso
+# [dmitriy-morozov.github.io/look](https://dmitriy-morozov.github.io/gustoso/)
+
+###Особенности проекта:
+* HTML5, CSS3, JQUERY;
+* Адаптивная вёрстка без Bootstrap;
+* Валидная разметка по методологии БЭМ;
+* Препроцессор LESS; 
+* Автоматизация сборки с помощью GULP;
+* Оптимизация стилей, изображений, скриптов. 
+
+
---
+++ js/accordeon.js
@@ -0,0 +1,26 @@
+ $('#accordeon-item--first').click(function() {
+            if($("#accordeon-item--first").is(':checked')) { 
+               $(".accordeon-item--first").css('z-index', '5');
+               $(".accordeon-item--second").css('z-index', '2');
+               $(".accordeon-item--third").css('z-index', '1');
+            };
+         
+         });
+         
+         $('#accordeon-item--second').click(function() {
+            if($("#accordeon-item--second").is(':checked')) { 
+               $(".accordeon-item--second").css('z-index', '5');
+               $(".accordeon-item--first").css('z-index', '1');
+               $(".accordeon-item--third").css('z-index', '2');
+            };
+         
+         });
+         
+         $('#accordeon-item--third').click(function() {
+            if($("#accordeon-item--third").is(':checked')) { 
+               $(".accordeon-item--third").css('z-index', '5');
+               $(".accordeon-item--first").css('z-index', '1');
+               $(".accordeon-item--second").css('z-index', '2');
+            };
+         
+         });
